1dc90cb171bdfcbf1b47448c135a76b1c6fe88e8
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-02-25 22:56:21 +0000

    Update StatTable.jsx

--- src/Components/StatTable.jsx
+++ src/Components/StatTable.jsx
@@ -6,7 +6,7 @@ const StatTable = ({title , icon , desc}) => {
       <div>
         <h1 className='text-xl font-bold align-center text-center'>{title}</h1>
       </div>
-      <div className='flex gap-2'>
+      <div className='flex gap-2  '>
         <img src={icon} alt="" />
         <p className='text-md font-semibold text-[#7A7A7A] '>{desc}</p>
       </div>
