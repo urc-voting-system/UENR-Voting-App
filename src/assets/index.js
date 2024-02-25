ca41f5d8000d95775769a7fde2056ce9fee1b038
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-02-25 22:59:54 +0000

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,12 @@ import calendar from './calendar.svg';
 import edit from './edit.svg'
 import grid from './grid.svg';
 import users from './users.svg';
-
+import crown from './crown.png'
+import help from './help.png'
+import home from './home.png'
+import logo from './logo.png'
+import records from './records.png'
+import stats from './stats.png'
 export
 {
     activity,
@@ -11,4 +16,11 @@ export
     edit,
     grid,
     users,
+    crown,
+    help,
+    home,
+    logo,
+    records,
+    stats,
+
 }
