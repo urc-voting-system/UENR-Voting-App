3f63c52ce1174906609a461e9a2bae53f0a9b705
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-04-11 00:35:48 +0000

    Implement search functionality for event names in Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
+import React, { useState } from "react";
 import "./Pages.scss";
 import Sidebar from "../Components/Sidebar";
 import { Link } from "react-router-dom";
-import { FaArrowRight, FaSearch } from "react-icons/fa";
-// import image from "../assets/image.png";
-// import img from "../assets/img.png";
+import { FaArrowRight } from "react-icons/fa";
 import try1 from "../assets/try1.png";
 
 const Home = ({ sidebarStatus, closeSidebar }) => {
+  const [searchInput, setSearchInput] = useState("");
+
   const eventData = [
     {
       image: try1,
@@ -26,6 +27,14 @@ const Home = ({ sidebarStatus, closeSidebar }) => {
     },
   ];
 
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const filteredEvents = eventData.filter((event) =>
+    event.title.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <div className="home">
       <Sidebar sidebarStatus={sidebarStatus} closeSidebar={closeSidebar} />
@@ -37,13 +46,15 @@ const Home = ({ sidebarStatus, closeSidebar }) => {
             <input
               type="text"
               className="w-full h-14 rounded-3xl border-2 border-[#474747] bg-transparent px-3 outline-none"
-              placeholder="Enter event name...  "
+              placeholder="Enter event name..."
+              value={searchInput}
+              onChange={handleInputChange}
             />
           </div>
 
           <div className="flex justify-center items-center">
             <div className="Events-Contianer pt-10 grid  grid-cols-1 lg:grid-cols-4  md:grid-cols-3  gap-10 ">
-              {eventData.map((event, index) => (
+              {filteredEvents.map((event, index) => (
                 <div
                   key={index}
                   className="bg-transparent  rounded-xl w-60 h-auto object-contain"
@@ -71,4 +82,5 @@ const Home = ({ sidebarStatus, closeSidebar }) => {
     </div>
   );
 };
+
 export default Home;
